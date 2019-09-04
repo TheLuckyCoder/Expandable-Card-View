c4804b06755d126fae396d39fece9536c6b13164
Razvan <razvan.filea@gmail.com>
2019-09-04 16:45:27 +0300

    Migrate to AndroidX

--- sample/src/main/java/net/theluckycoder/expandablecardview/sample/MainActivity.kt
+++ sample/src/main/java/net/theluckycoder/expandablecardview/sample/MainActivity.kt
@@ -1,9 +1,9 @@
 package net.theluckycoder.expandablecardview.sample
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import net.theluckycoder.expandablecardview.ExpandableCardView
 
 class MainActivity : AppCompatActivity() {
